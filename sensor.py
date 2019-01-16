003abb373b4cbbaa6019883737d93aaac0ae5718
neolao <contact@neolao.com>
2019-01-16 23:40:41 +0100

    Add sensor service

--- sensor.py
+++ sensor.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python
 import RPi.GPIO as GPIO
 import time
+import urllib2
 
-#GPIO SETUP
-channel = 15
+SensorPin = 15
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(channel, GPIO.IN)
+GPIO.setup(SensorPin, GPIO.IN)
 
 def callback(channel):
-        if GPIO.input(channel):
-                print "Sound Detected!"
-        else:
-                print "Sound Detected!"
+        urllib2.urlopen("http://10.0.1.36:8080")
 
-GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)  # let us know when the pin goes HIGH or LOW
-GPIO.add_event_callback(channel, callback)  # assign function to GPIO PIN, Run function on change
+GPIO.add_event_detect(SensorPin, GPIO.BOTH, bouncetime=300)  # let us know when the pin goes HIGH or LOW
+GPIO.add_event_callback(SensorPon, callback)  # assign function to GPIO PIN, Run function on change
 
-# infinite loop
 while True:
         time.sleep(1)
+
