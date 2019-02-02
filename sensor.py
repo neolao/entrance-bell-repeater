f43341224577f1c39fb1ad2e17c9f8d4f3767946
neolao <contact@neolao.com>
2019-02-02 23:17:25 +0100

    Update sensor.py

--- sensor.py
+++ sensor.py
@@ -11,11 +11,29 @@ file = open("/boot/buzz-ip.txt", "r")
 buzzIp = file.read().strip()
 file.close()
 
-def callback(channel):
-        urllib2.urlopen("http://" + buzzIp + ":8080")
 
-GPIO.add_event_detect(SensorPin, GPIO.BOTH, bouncetime=300)  # let us know when the pin goes HIGH or LOW
-GPIO.add_event_callback(SensorPin, callback)  # assign function to GPIO PIN, Run function on change
+lastCallback = 0
+consecutiveCallback = 0
+def callback(channel):
+  global lastCallback
+  global consecutiveCallback
+  global buzzIp
+
+  currentTime = int(round(time.time() * 1000))
+  if currentTime - lastCallback < 500:
+    consecutiveCallback += 1
+  else:
+    consecutiveCallback = 0
+
+  if consecutiveCallback == 1:
+    print("buzz")
+    urllib2.urlopen("http://" + buzzIp + ":8080")
+
+  lastCallback = currentTime
+
+  print(str(lastCallback) + " : " + str(consecutiveCallback))
+
+GPIO.add_event_detect(SensorPin, GPIO.BOTH, callback, bouncetime=300)
 
 while True:
         time.sleep(1)
